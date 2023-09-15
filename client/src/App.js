2d41e0afbadbc006ea1561e911ed246744a84553
rohitchauhan533 <chauhanrohit533@gmail.com>
2023-09-15 22:27:42 +0530

    createtodo functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter, Route, Routes }  from 'react-router-dom';
 import './App.scss';
+import { CreateTodo } from './components/createTodo';
+import { ShowTodoList } from './components/showTodoList';
 
 function App() {
   return (
-    <div className="App">
-    <header>Hello world</header>
+    <div className="app-contents">
+    <BrowserRouter>
+      <Routes>
+      <Route exact path="/" component={ ShowTodoList } />
+      <Route exact path="/" component={ CreateTodo } />
+      </Routes>
+
+    </BrowserRouter>
     </div>
   );
 }
